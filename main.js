d6ba7ee8c74566203036bfc3b16651a1721b8a5c
Santino Scannapieco <santinoscannapieco@gmail.com>
2023-10-26 02:37:30 -0300

    Cambie el código de alerts por uno con relacion e interaccion con HTML(todavia no esta terminado)

--- main.js
+++ main.js
@@ -1,172 +1,198 @@
-alert(`La página es para que el usuario registre sus gastos diarios
-Usuarios disponibles:
-cuenta1 123
-cuenta2 047`)
-
 // Clase Usuario con sus metodos
 class Usuario {
-    constructor(nombre, password, categorias, gastos) {
+    constructor(nombre, categorias) {
         this.nombre = nombre
-        this.password = password
         this.categorias = categorias
-        this.gastoTotal = gastos
-        
+        this.gastoTotal = this.sumarGasto(categorias)
     }
 
-    sumarCategoria(newCategoria) {
-        this.categorias.push(newCategoria)
+    sumarCategoria(clave, valor) {
+        this.categorias[clave] = valor
+        this.gastoTotal = this.sumarGasto(this.categorias)
         console.log(this.categorias)
     }
     restarCategoria() {    }
 
-    sumarGasto(newGasto) {
-        this.gastoTotal += newGasto
+    sumarGasto(categorias) {
+        let suma = 0
+
+        for (let clave in categorias) {
+            if (categorias.hasOwnProperty(clave)) {
+                suma += categorias[clave]
+            }
+        }
+        return suma
     }
     restarGasto() {    }
+
+    obtenerCategorias() {
+        return this.categorias
+    }
 }
 
+let usuario
+let contenedor = document.createElement('div')
+contenedor.id = 'contenedor'
+document.body.appendChild(contenedor)
 
-// Datos de cuentas ya creada
-const cuenta1 = new Usuario("cuenta1", "123", ["Comida", "Regalos"], 20152)
-const cuenta2 = new Usuario("cuenta2", "047", ["Salidas", "Ropa"], 52000)
+function mostrarInicio() {
+    contenedor.innerHTML = `<h3>Ingresa tu nombre</h3>
+                            <input id="nombre" placeholder="Nombre">
+                            <button id="btn">Guardar nombre</button>`
 
-
-// Funciones
-function signIn() {
-    let usuarioNuevo = prompt("Ingrese un usuario")
-    let contraseñaNueva = prompt("Ingrese una contraseña")
-
-    return new Usuario(usuarioNuevo, contraseñaNueva, [], 0)
+    document.getElementById('btn').addEventListener('click', guardarNombre)
 }
-function logIn() {
-    let usuarioIngresado = prompt("Ingrese su usuario")
-    let contraseñaIngresada = prompt("Ingrese su contraseña")
-    return [ usuarioIngresado, contraseñaIngresada ]
+
+function mostrarMenu() {
+    contenedor.innerHTML = `<h3>Bienvenido, ${usuario.nombre}!</h3>
+                            <p>Elija una de las opciones para avanzar</p>
+                            <p>Gastos Totales: ${usuario.gastoTotal}</p>
+                            <button id="verCat">Ver Categorías</button>
+                            <button id="agregarCat">Agregar categoría nueva</button>
+                            <button id="agregarGasto">Agregar gasto nuevo</button>`
+
+    document.getElementById('verCat').addEventListener('click', verCategorias)
+    document.getElementById('agregarCat').addEventListener('click', agregarCategoria)
+    document.getElementById('agregarGasto').addEventListener('click', agregarGasto)
+
 }
-function chequeoCuentas(usuarioIngresado, contraseñaIngresada) {
-    if (usuarioIngresado === cuenta1.nombre && contraseñaIngresada === cuenta1.password) {
-        user = cuenta1
-        return [ true, user ]
-    }else if(usuarioIngresado === cuenta2.nombre && contraseñaIngresada === cuenta2.password) {
-        user = cuenta2
-        return [ true, user ]
-    }else {
-        return [ false ]
+
+function guardarNombre() {
+    const nombre = document.getElementById('nombre').value
+
+    if(nombre === "") {
+        Swal.fire({
+            icon: 'warning',
+            text: 'Completá el campo con tu nombre',
+            timer: 2000,
+            timerProgressBar: true,
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+            showConfirmButton: false
+        })
+        return
     }
 
-
-    /* else if(usuarioIngresado === arraySignIn.nombre && contraseñaIngresada === arraySignIn.password){
-        
-    } */
+    console.log(nombre)
+    usuario = new Usuario(nombre, {"salidas": 2000, "ropa": 5000, "comida": 10000})
+    mostrarMenu()
 }
 
-let flujo = "eleccion 1"
-let usuarioIngresado
-let contraseñaIngresada
-let usuario2
-let contraseña2
-let respuesta
-let arraySignIn
-let arrayLogIn
+function verCategorias() {
+    let categorias = usuario.obtenerCategorias()
+    let categoriasHTML = ''
 
-let user
-
-do {
-    switch (flujo) {
-        case "eleccion 1":
-            // Elegir entre iniciar sesión o registrarse
-            let eleccion1 = prompt("Elegir 1 o 2\n1. Registrarse\n2. Iniciar sesion")
-            switch (eleccion1) {
-                case "1":
-                    flujo = "signIn"
-                    break
-                case "2":
-                    flujo = "logIn"
-                    break
-                default:
-                    alert("No responde la pregunta")
-                    break
-                case null:
-                    flujo = "salir"
-                    break
-            }
-            break
-        case "signIn":
-            /* alert("Ingresá los siguientes datos para registrarte")
-            arraySignIn = signIn() 
-            flujo = "logIn" */
-
-            alert("Todavía no está disponible hacer un nuevo registro")
-
-            flujo = "eleccion 1"
-            break
-        case "logIn":
-            alert("Ingresá los siguientes datos para inicia sesion")
-            arrayLogIn = logIn()
-            
-            // Mando usuario ingresado y contraseña ingresada y recibo en respuesta[0] si la cuenta existe y en respuesta[1] el usuario con todos los datos
-            respuesta = chequeoCuentas(arrayLogIn[0], arrayLogIn[1])
-            if (respuesta[0] == true) {
-                flujo = "bienvenido"
-            }else { 
-                alert("El usuario y contraseña no coinciden") 
-                flujo = "eleccion 1"
-            }
-            user = respuesta[1]
-            break
-        case "bienvenido":
-            alert("Bienvenido " + user.nombre)
-            flujo = "eleccion 2"
-            break
-        case "eleccion 2":
-            let eleccion2 = prompt(`Elegí que queres hacer ${user.nombre} \n1. Ver mis gastos\n2. Agregar nueva categoria\n3. Agregar nuevo gasto\n4. Cerrar sesión\n5. Salir`)
-
-            switch(eleccion2) {
-                case "1":
-                    // Ver gastos x categorias
-                    alert("$" + user.gastoTotal)
-                    break
-                case "2":
-                    // Ver categorias
-                    // Agregar categorias nuevas
-                    cadena = user.categorias.join("\n")
-                    newCategoria = prompt( cadena )
-
-                    if (newCategoria != "" && newCategoria != null) {
-                        user.sumarCategoria(newCategoria)
-                    }else {
-                        alert("No completaste el campo")
-                    }
-                    break
-                case "3":
-                    // Agregar gasto nuevo
-                    // PROXIMA ENTREGA: AGREGAR QUE EL GASTO SE PONGA A UNA CATEGORIA ESPECÍFICA
-                    newGasto = parseInt(prompt("Agregar gasto nuevo"))
-                    console.log(newGasto)
-                    if(newGasto > 0 && newGasto < 500000) {
-                        user.sumarGasto(newGasto)
-                    }else {
-                        alert("Ese monto no está disponible")
-                    }
-                    break
-                case "4":
-                    // cerrar sesion
-                    flujo = "eleccion 1"
-                    break
-                case "5":
-                    // salir
-                    flujo = "salir"
-                    break
-                default:
-                    alert("No responde la pregunta")
-                    break
-                case null:
-                    flujo = "salir"
-                    break
-            }
-        break
-        case "salir":
-            //alert("¿Seguro que queres salir?")
-            break
+    if (Object.keys(categorias).length === 0) {
+        Swal.fire({
+            icon: 'warning',
+            text: 'No tenes categorias creadas',
+            timer: 2000,
+            timerProgressBar: true,
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+            showConfirmButton: false
+        })
+    } else {
+        for (let categoria in categorias) {
+            categoriasHTML += `<p>${categoria}: ${categorias[categoria]}</p>`
+        }
     }
-}while (flujo != "salir")
+
+    contenedor.innerHTML = `<h3>Categorías</h3>${categoriasHTML}<br>
+                            <button id="volver">Volver</button>`
+
+    document.getElementById('volver').addEventListener('click', mostrarMenu)
+}
+
+function agregarCategoria() {
+    contenedor.innerHTML = `<h3>Agregar categoría</h3>
+                            <p>Ingrese los siguiente datos para crear una categoía nueva</p>
+                            <input id="nombreCat" placeholder="Categoría">
+                            <input id="gastoCat" placeholder="gasto">
+                            <button id="crearCatBtn">Crear categoría</button><br>
+                            <button id="volver">Volver</button>`
+
+    document.getElementById('crearCatBtn').addEventListener('click', function() {
+        let nombreCat = document.getElementById('nombreCat').value
+        let gastoCat = parseInt(document.getElementById('gastoCat').value)
+
+        // Itero las categorias para no crear 2 categorias con el mismo nombre
+        for (let categoria in usuario.categorias){
+            if(categoria === nombreCat) {
+                Swal.fire({
+                    icon: 'warning',
+                    text: 'Esa categoria ya existe',
+                    timer: 2000,
+                    timerProgressBar: true,
+                    allowOutsideClick: false,
+                    allowEscapeKey: false,
+                    allowEnterKey: false,
+                    showConfirmButton: false
+                })
+                return
+            }
+        }
+
+        if (nombreCat && !isNaN(gastoCat)) {
+            usuario.sumarCategoria(nombreCat, gastoCat)
+            mostrarMenu()
+        } else {
+            Swal.fire({
+                icon: 'warning',
+                text: 'Completá todos los campos por favor',
+                timer: 2000,
+                timerProgressBar: true,
+                allowOutsideClick: false,
+                allowEscapeKey: false,
+                allowEnterKey: false,
+                showConfirmButton: false
+            })
+        }
+    })
+
+    document.getElementById('volver').addEventListener('click', mostrarMenu)
+}
+
+function agregarGasto() {
+    let categorias = usuario.obtenerCategorias()
+    let categoriasSelectHTML = `<option value="">Categorias</option>`
+    
+    for (let categoria in categorias) {
+        categoriasSelectHTML += `<option value="${categoria}">${categoria}</option>`
+    }
+
+    contenedor.innerHTML = `<h3>Agregar gasto nuevo</h3>
+                            <p>Ingrese los siguientes datos para agregar un nuevo gasto</p>
+                            <select id="categoriasSelect">${categoriasSelectHTML}</select>
+                            <input id="gastoNuevo" placeholder="Gasto">
+                            <button id="crearGastoBtn">Crear gasto</button><br>
+                            <button id="volver">Volver</button>`;
+
+    document.getElementById('crearGastoBtn').addEventListener('click', function() {
+        let categoriaSeleccionada = document.getElementById('categoriasSelect').value
+        let gastoNuevo = parseInt(document.getElementById('gastoNuevo').value)
+
+        if (categoriaSeleccionada != '' && !isNaN(gastoNuevo)) {
+            // Actualizar el gasto en la categoría seleccionada
+            usuario.sumarCategoria(categoriaSeleccionada, gastoNuevo)
+            mostrarMenu()
+        } else {
+            Swal.fire({
+                icon: 'warning',
+                text: 'Los datos ingresados no son válidos',
+                timer: 2000,
+                timerProgressBar: true,
+                allowOutsideClick: false,
+                allowEscapeKey: false,
+                allowEnterKey: false,
+                showConfirmButton: false
+            });
+        }
+    });
+
+    document.getElementById('volver').addEventListener('click', mostrarMenu)
+}
+
+mostrarInicio()
