67fa0e15489cc8f18e34d1fb0bc503d080e96c5a
Santino Scannapieco <santinoscannapieco@gmail.com>
2023-11-08 14:59:19 -0300

    Agrego LogIn y SignIn, falta terminar

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // Clase Usuario con sus metodos
 class Usuario {
-    constructor(nombre, categorias) {
-        this.nombre = nombre
+    constructor(username, password, categorias) {
+        this.username = username
+        this.password = password
         this.categorias = categorias
         this.gastoTotal = this.sumarGastoTotal(categorias)
     }
@@ -11,7 +12,6 @@ class Usuario {
         nuevaCategoria[clave] = valor
         this.categorias.push(nuevaCategoria)
         this.gastoTotal = this.sumarGastoTotal(this.categorias)
-        console.log(this.categorias)
     }
 
     sumarGastoCat(clave, valor) {
@@ -44,49 +44,107 @@ let contenedor = document.createElement('div')
 contenedor.id = 'contenedor'
 document.body.appendChild(contenedor)
 
+let traerUsuario = JSON.parse(localStorage.getItem("usuarioLoggeado")) || ''
 
-let datos = JSON.parse(localStorage.getItem("usuario")) || ''
+//traerUsuario === '' ? mostrarInicio() :  usuario = new Usuario(traerUsuario.username, traerUsuario.password, traerUsuario.categorias), mostrarMenu()
 
-if(datos === '') {
+if ( traerUsuario === '' ) {
     mostrarInicio()
-}else {
-    usuario = new Usuario(datos.nombre, datos.categorias)
+} else {
+    usuario = new Usuario(traerUsuario.username, traerUsuario.password, traerUsuario.categorias)
     mostrarMenu()
 }
+
+console.log(traerUsuario)
 
 function mostrarInicio() {
-    contenedor.innerHTML = `<h3>Ingresa tu nombre</h3>
-                            <input id="nombre" placeholder="Nombre">
-                            <button id="btn">Guardar nombre</button>`
+    contenedor.innerHTML = `<h1>BIENVENIDO</h1>
+                            <button id="logIn">logIn</button>
+                            <button id="signIn">signIn</button>`
 
-    document.getElementById('btn').addEventListener('click', guardarNombre)
+    // creo los eventos
+    document.getElementById("logIn").addEventListener("click", logIn)
+    document.getElementById("signIn").addEventListener("click", signIn)
 }
 
-function guardarNombre() {
-    const nombre = document.getElementById('nombre').value
+function signIn() {
+    contenedor.innerHTML = `<h3>Completá el formulario para crear tu cuenta</h3>
+                            <input id="username" placeholder="Nombre" required>    
+                            <input id="password" placeholder="Password" required>
 
-    if(nombre === "") {
-        Swal.fire({
-            icon: 'warning',
-            text: 'Completá el campo con tu nombre',
-            timer: 2000,
-            timerProgressBar: true,
-            allowOutsideClick: false,
-            allowEscapeKey: false,
-            allowEnterKey: false,
-            showConfirmButton: false
-        })
+                            <button id="botonRegistrar">Registrarme</button>
+                            <button id="botonVolver">Volver</button>`
+
+    document.getElementById("botonRegistrar").addEventListener("click", crearCuenta)
+    document.getElementById("botonVolver").addEventListener("click", mostrarInicio)
+}
+
+function crearCuenta() {
+    let username = document.getElementById('username').value
+    let password = document.getElementById('password').value
+
+    // Comprobar si ya existe un usuario con el mismo nombre
+    let users = JSON.parse(localStorage.getItem('users')) || []
+    let existingUser = users.find(function(user) {
+        return user.username === username
+    })
+
+    if (existingUser) {
+        mostrarAlert('error', 'El usuario ya existe. Por favor, elige otro nombre de usuario.', 2000)
         return
-    }
+    } else if (username && password){
+        // Si el usuario no existe, lo registramos
+        usuario = new Usuario(username, password, [], 0)
+        localStorage.setItem('usuarioLoggeado', JSON.stringify(usuario))
 
-    console.log(nombre)
-    usuario = new Usuario(nombre, [])
-    guardarLocalStorage("usuario", JSON.stringify(usuario))
-    mostrarMenu()
+        users.push(usuario)
+
+        localStorage.setItem('users', JSON.stringify(users))
+        mostrarMenu()
+        mostrarAlert('success', `Felicitaciones ${usuario.username} se completó tu registro`, 2000)
+    } else {
+        mostrarAlert('warning', 'Completa los campos', 2000)
+    }
+}
+
+
+function logIn() {
+    contenedor.innerHTML =  `<h3>Inicie sesión con su cuenta existente</h3>
+                            <input id="loginUsername" placeholder="Nombre" required>
+                            <input id="loginPassword" placeholder="Password" required>
+          
+                            <button id="botonLogIn">Iniciar sesión</button>
+                            <button id="botonVolver">Volver</button>`
+
+    document.getElementById("botonLogIn").addEventListener("click", chequearCuenta)
+    document.getElementById("botonVolver").addEventListener("click", mostrarInicio)
+}
+
+function chequearCuenta() {
+    let loginUsername = document.getElementById('loginUsername').value
+    let loginPassword = document.getElementById('loginPassword').value
+
+    // Obtener usuarios almacenados en localStorage
+    let users = JSON.parse(localStorage.getItem('users')) || []
+    let loggedInUser = users.find(function(user) {
+        return user.username === loginUsername && user.password === loginPassword
+    })
+
+    if (loggedInUser) {
+        usuario = new Usuario(loggedInUser.username, loggedInUser.password, loggedInUser.categorias)
+        localStorage.setItem('usuarioLoggeado', JSON.stringify(usuario))
+        console.log('Usuario actual:', usuario)
+        mostrarMenu()
+        alert('Inicio de sesión exitoso.')
+    } else if (loginUsername && loginPassword){
+        mostrarAlert('error', 'Credenciales incorrectas. Por favor, inténtalo de nuevo.', 0)
+    } else {
+        mostrarAlert('warning', 'Completa los campos', 2000)
+    }
 }
 
 function mostrarMenu() {
-    contenedor.innerHTML = `<h3>Bienvenido, ${usuario.nombre}!</h3>
+    contenedor.innerHTML = `<h3>Bienvenido, ${usuario.username}!</h3>
                             <p>Elija una de las opciones para avanzar</p>
                             <p>Gastos Totales: $ ${usuario.gastoTotal}</p>
                             <button id="verCat">Ver Categorías</button>
@@ -104,16 +162,7 @@ function verCategorias() {
     let categoriasHTML = ''
 
     if (categorias.length === 0) {
-        Swal.fire({
-            icon: 'warning',
-            text: 'No tenes categorias creadas',
-            timer: 2000,
-            timerProgressBar: true,
-            allowOutsideClick: false,
-            allowEscapeKey: false,
-            allowEnterKey: false,
-            showConfirmButton: false
-        })
+        mostrarAlert('warning', 'No tenes categorias creadas', 2000)
         return
     } else {
         for (let categoria of categorias) {
@@ -141,43 +190,27 @@ function agregarCategoria() {
         let nombreCat = document.getElementById('nombreCat').value
         let gastoCat = parseInt(document.getElementById('gastoCat').value)
 
-        // Itero las categorias para no crear 2 categorias con el mismo nombre
-        for (let categoria in usuario.categorias){
-            if(categoria === nombreCat) {
-                Swal.fire({
-                    icon: 'warning',
-                    text: 'Esa categoria ya existe',
-                    timer: 2000,
-                    timerProgressBar: true,
-                    allowOutsideClick: false,
-                    allowEscapeKey: false,
-                    allowEnterKey: false,
-                    showConfirmButton: false
-                })
-                return
-            }
+        let categoriasGuardadas = usuario.categorias
+        let existingCategorias = categoriasGuardadas.find( function(c) {
+            return c[nombreCat] !== undefined
+        })
+
+        if (existingCategorias) {
+            mostrarAlert('warning', 'Esa categoria ya existe', 2000)
+            return
         }
+        
+
 
         if (nombreCat && !isNaN(gastoCat)) {
             usuario.sumarCategoria(nombreCat, gastoCat)
-            guardarLocalStorage("usuario", JSON.stringify(usuario))
+            guardarLocalStorage('usuarioLoggeado', JSON.stringify(usuario))
 
             mostrarMenu()
         } else {
-            Swal.fire({
-                icon: 'warning',
-                text: 'Completá todos los campos por favor',
-                timer: 2000,
-                timerProgressBar: true,
-                allowOutsideClick: false,
-                allowEscapeKey: false,
-                allowEnterKey: false,
-                showConfirmButton: false
-            })
+            mostrarAlert('warning', 'Completá todos los campos por favor', 2000)
         }
     })
-
-
     document.getElementById('volver').addEventListener('click', mostrarMenu)
 }
 
@@ -206,26 +239,37 @@ function agregarGasto() {
 
         if (categoriaSeleccionada != '' && !isNaN(gastoNuevo)) {
             usuario.sumarGastoCat(categoriaSeleccionada, gastoNuevo)
-            guardarLocalStorage("usuario", JSON.stringify(usuario))
+            guardarLocalStorage('usuarioLoggeado', JSON.stringify(usuario))
 
             mostrarMenu()
         }else {
-            Swal.fire({
-                icon: 'warning',
-                text: 'Los datos ingresados no son válidos',
-                timer: 2000,
-                timerProgressBar: true,
-                allowOutsideClick: false,
-                allowEscapeKey: false,
-                allowEnterKey: false,
-                showConfirmButton: false
-            })
+            mostrarAlert('warning', 'Los datos ingresados no son válidos', 2000)
         }
     })
     document.getElementById('volver').addEventListener('click', mostrarMenu)
 }
 
-
 function guardarLocalStorage(clave,valor) {
     localStorage.setItem(clave, valor)
+}
+
+function mostrarAlert(icono, texto, tiempo) {
+    if (tiempo != 0) {
+        Swal.fire({
+            icon: icono,
+            text: texto,    
+            timer: tiempo,
+            timerProgressBar: true,
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+            showConfirmButton: false
+        })
+    }else {
+        Swal.fire({
+            icon: icono,
+            text: texto,
+        })
+    }
+    
 }
